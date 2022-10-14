196000917d25ae42ef2d64580407cd7c3d48d4ee
Dennis H <111554091+frozzel@users.noreply.github.com>
2022-10-13 23:26:03 -0400

    maybe now

--- server.js
+++ server.js
@@ -2,18 +2,17 @@
 const fs = require( "fs" );
 const express = require( "express" );
 const path = require( "path" );
-const { json } = require("express");
 const notes= JSON.parse(fs.readFileSync("db/db.json", "utf8"));
 
 
 ///// Init Server & middle Ware ////
 const app = express();
-const PORT = process.envPORT || 3001;
+const PORT = process.env.PORT || 3001;
 app.use( express.urlencoded({ extended: true }));
 app.use( express.json());
 app.use( express.static( "public" ));
 
-
+///////// Write Notes /////////////
 app.post("/api/notes", (req, res)=>{
     let newNote = req.body;
     notes.push(newNote);
@@ -21,14 +20,10 @@ app.post("/api/notes", (req, res)=>{
     res.json(notes);
 })
 
-
-
-
 //// links to DB /////
 app.get("/api/notes", (req, res)=>{
     return res.json(notes)
 });
-
 
 //// Url for notes html///
 app.get("/notes", (req, res)=> res.sendFile(path.join(__dirname, "./public/notes.html")));
@@ -38,6 +33,4 @@ app.get('/', (req, res)=> res.sendFile(path.join(__dirname, "./public/index.html
 app.get('*', (req, res)=> res.sendFile(path.join(__dirname, "./public/index.html")));
 
 //// Magic ///////
-app.listen(3001, ()=>{
-    console.log(`App listening at http://localhost:${PORT}`)
-})
+app.listen(3001, ()=> console.log(`App listening at http://localhost:${PORT}`))
